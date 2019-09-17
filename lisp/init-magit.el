77d9f6bdfc0afba95031cc822f60f30bd053e233
Tim Quelch <tim@quelch.name>
2019-09-17 14:39:19 +1000

    Add more git functionality

--- lisp/init-magit.el
+++ lisp/init-magit.el
@@ -12,6 +12,10 @@
   :config
   (set-face-attribute 'magit-branch-current nil :box t))
 
+(use-package forge
+  :after magit
+  :demand)
+
 (use-package evil-magit
   :after magit
   :demand)
@@ -24,6 +28,10 @@
 
   (with-eval-after-load 'magit
     (add-hook 'magit-post-refresh-hook 'diff-hl-magit-post-refresh)))
+
+(use-package gitignore-mode)
+(use-package gitattributes-mode)
+(use-package gitconfig-mode)
 
 (provide 'init-magit)
 ;;; init-magit.el ends here
