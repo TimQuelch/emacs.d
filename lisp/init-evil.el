3b7db9e2d35fecd23acbf9ea23ba7022d598818e
Tim Quelch <tim@quelch.name>
2019-09-26 11:30:17 +1000

    Rebind C-S-D to scroll up

--- lisp/init-evil.el
+++ lisp/init-evil.el
@@ -15,7 +15,8 @@
          ([remap evil-previous-line] . evil-previous-visual-line)
          :map evil-motion-state-map
          ([remap evil-next-line] . evil-next-visual-line)
-         ([remap evil-previous-line] . evil-previous-visual-line))
+         ([remap evil-previous-line] . evil-previous-visual-line)
+         ("C-S-D" . evil-scroll-up))
   :init
   (setq evil-cross-lines t)
   :config
