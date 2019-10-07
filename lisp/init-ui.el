dfe62afcee839fb8821a84abd12252eaba119e1b
Tim Quelch <tim@quelch.name>
2019-10-07 17:28:26 +1000

    Only use line numbers in prog and text modes

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -45,7 +45,7 @@
 ;; Turn on line numbers
 (use-package display-line-numbers
   :ensure nil
-  :hook (after-init . global-display-line-numbers-mode)
+  :hook ((prog-mode text-mode) . display-line-numbers-mode)
   :config
   (set-face-attribute 'line-number nil :inherit '(fixed-pitch))
   (set-face-attribute 'line-number-current-line nil :inherit '(fixed-pitch hl-line)))
