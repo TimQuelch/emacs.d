99b67ed7df5b7831143ac7557e64cfe1df73f077
Tim Quelch <timothy.quelch@versent.com.au>
2025-02-14 17:46:25 +1100

    Use aidermacs instead of aider.el

--- packages.el
+++ packages.el
@@ -33,7 +33,7 @@
 
 (package! sops :pin "46548b854b35983b2e9e5eb4276634dfc41abfa0")
 
-(package! aider :recipe (:host github :repo "tninja/aider.el" :files ("aider.el" "aider-doom.el")))
+(package! aider :recipe (:host github :repo "MatthewZMD/aidermacs" :files ("aider.el" "aider-doom.el")))
 
 (if (tq/get-config 'load-ssh-agent-from-shell-env nil)
     (package! exec-path-from-shell :pin "72ede29a0e0467b3b433e8edbee3c79bab005884")
